6175c1e4a241c42b4e8a33be057ed15d3509b6a0
Poh Peng Wang <wang_poh_peng@tech.gov.sg>
2019-01-22 16:49:35 +0800

    added default admin record

--- server/services/admin.js
+++ server/services/admin.js
@@ -3,6 +3,19 @@ const model = models.Administrator;
 
 module.exports = {
 
+    getAll: function() {
+        return model.findAll({
+            order: [
+                ['id', 'DESC']
+            ]
+        })
+    },
+    createAdmin: function(body) {
+        return model.create({
+            username: body.username,
+            password: body.password,
+        })
+    },
     getAdminForAuth: function(body) {
         return model.findAll({
             where: {
